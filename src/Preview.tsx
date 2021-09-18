4dcd80591dfff0e105a47a4a4cd1326b3bac0a5b
takurinton <takurinton@takurinton.com>
2021-09-18 17:58:40 +0900

    fix

--- src/Preview.tsx
+++ src/Preview.tsx
@@ -1,27 +1,14 @@
-import { ASTNode, parse } from "graphql";
-import { useState } from "preact/hooks";
-import { NodeContextProvider } from "./context";
+import { ASTNode } from "graphql";
 
 export const Preview = (
   { 
     query, 
-    ast, 
-    onChange
+    ast 
   }: { 
     query: string, 
     ast: ASTNode, 
     onChange: (query: string, ast: ASTNode) => void
 }) => {
-  const [q, setQ] = useState<string>(query);
-  const [a, setA] = useState<ASTNode>(ast)
-
-  const onChangeQuery = ({ currentTarget }: JSX.TargetedEvent<HTMLInputElement, Event>) => {
-    setQ(currentTarget.value);
-    const _a = parse(q)
-    setA(_a)
-    onChange(q, _a);
-  };
-
   return (
     <div style={
       {
@@ -30,19 +17,11 @@ export const Preview = (
         paddingLeft: '50px'
       }
     }>
-      {/* query から form をいじるやつやりたい */}
-      <NodeContextProvider
-      root={ast}
-      onChangeNode={newDocument => {
-        const astnode = newDocument as ASTNode;
-      }}
-      >
-        <pre style={{ background: '#FFFFF0' }}>
-          <textarea rows="30" value={q} onInput={onChangeQuery}>
-              {query}
-          </textarea>
-        </pre>
-      </NodeContextProvider>
+      <pre style={{ background: '#FFFFF0' }}>
+        <code>
+            {query}
+        </code>
+      </pre>
     </div>
   );
 }
